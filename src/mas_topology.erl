49b48f852f16ded38b57537d67d5e669d520789f
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-02-13 17:14:15 +0100

    Add ring topology

--- src/mas_topology.erl
+++ src/mas_topology.erl
@@ -13,7 +13,20 @@
 %%%=============================================================================
 
 destination(_Topology, _From, Nodes) when length(Nodes) < 2 ->
-    {error, no_destination};
+    no_destination;
 destination(mesh, From, Nodes) ->
     Destinations = [Node || Node <- Nodes, Node =/= From],
-    {ok, mas_utils:sample(Destinations)}.
+    {ok, mas_utils:sample(Destinations)};
+destination(ring, From, Nodes) ->
+    N = length(Nodes),
+    FromIdx = mas_utils:index_of(From, Nodes),
+    NextIdx = case rand:uniform() < 0.5 of
+                  true  -> FromIdx + 1;
+                  false -> FromIdx - 1
+              end,
+    DestinationIdx = case NextIdx of
+                         Idx when Idx > N -> 1;
+                         Idx when Idx < 1 -> N;
+                         Idx              -> Idx
+                     end,
+    {ok, lists:nth(DestinationIdx, Nodes)}.
