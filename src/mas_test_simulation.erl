8d3b4aca424f1ca86c1c27c69a646c7d650d5715
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-03-15 20:36:50 +0100

    Remove debug calls

--- src/mas_test_simulation.erl
+++ src/mas_test_simulation.erl
@@ -6,12 +6,9 @@
          simulation_teardown/1,
          simulation_result/2]).
 
-simulation_setup(_SP) ->
-    io:format("Simulation setup~n").
+simulation_setup(_SP) -> ok.
 
-simulation_teardown(_SP) ->
-    io:format("Simulation teardown~n").
+simulation_teardown(_SP) -> ok.
 
 simulation_result(_SP, Agents) ->
-    io:format("Number of results:~p~n", [length(Agents)]),
-    Agents.
+    {Agents, length(Agents)}.
